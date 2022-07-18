5ff86001cdc1468e5c09a5c5e01008732c08e694
Paul McGinley <Paulmcginley16@outlook.com>
2022-07-18 14:56:10 +0100

    [Model/Expense] DataAnnotations seems to be creating a problem for Expense index page to load because of the Model/Expense - SQL NUll reference happens. I have removed the annotations for now - However, the issue is that EF & SMSS has their own constraints. Will come back to this issue

--- InAndOut/Models/Expense.cs
+++ InAndOut/Models/Expense.cs
@@ -8,13 +8,11 @@ namespace InAndOut.Models
         [Key] //Sets a primary key
         public int ID { get; set; }
 
-    /*    [DisplayName("Expense")]
-        [Required] //Validation - Must be entered in */
-        [StringLength(30, MinimumLength = 3, ErrorMessage ="Expense name must be minium of 3 characters, and max of 30")]
+       /* [DisplayName("Expense")]
+        [Required]*/
         public string ExpenseName { get; set; }
-
-        [Required]
-    /*    [Range(1, int.MaxValue,ErrorMessage ="Amount must be greater than zero")]*/
+       /* [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Amount must be greater than 0!")]*/
         public int Amount { get; set; }
     }
 }
