66605203d3320be68bc2767ea99d5cff982c21d9
Paul McGinley <Paulmcginley16@outlook.com>
2022-07-19 09:49:35 +0100

    [Data/Migration] Migration added to SSMS, and database updated.
    Putting in my own test value for testing purposes

---
+++ InAndOut/Migrations/20220719084842_ExpenseTypeToDB.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InAndOut.Migrations
+{
+    public partial class ExpenseTypeToDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Expenses",
+                table: "Expenses");
+
+            migrationBuilder.RenameTable(
+                name: "Expenses",
+                newName: "Expense");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Expense",
+                table: "Expense",
+                column: "ID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Expense",
+                table: "Expense");
+
+            migrationBuilder.RenameTable(
+                name: "Expense",
+                newName: "Expenses");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Expenses",
+                table: "Expenses",
+                column: "ID");
+        }
+    }
+}
